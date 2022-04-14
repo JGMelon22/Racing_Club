cdbd107d73fde734f65c1ab0addfaa51ea964b44
JGMelon22 <jgvasconcellos22@gmail.com>
2022-04-14 16:38:00 -0300

    Building the club and race views, polishing them with some bootstrap designer, treating the controllers return and creating links at the home navbar to call them

--- Racing_Club/Controllers/ClubController.cs
+++ Racing_Club/Controllers/ClubController.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Racing_Club.Models;
 
 namespace Racing_Club.Controllers
 {
     public class ClubController : Controller
     {
+        // Dep. Injection
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            List<Club> clubs = _context.Clubs.ToList();
+            return View(clubs);
         }
     }
 }
--- Racing_Club/Controllers/RaceController.cs
+++ Racing_Club/Controllers/RaceController.cs
@@ -1,12 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
+using Racing_Club.Models;
 
 namespace Racing_Club.Controllers
 {
     public class RaceController : Controller
     {
+        // Dep. Injection
+        private readonly ApplicationDbContext _context;
+
+        public RaceController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        
         public IActionResult Index()
         {
-            return View();
+            List<Race> races = _context.Races.ToList();
+            return View(races);
         }
     }
 }
